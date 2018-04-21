69a5a94032d7af29c35ab81792163f8832b18623
JohnLin <john.lin0420@gmail.com>
2018-04-22 00:08:02 +0800

    fetch meta

--- src/main/java/com/example/blockgraph/BlockWritter.java
+++ src/main/java/com/example/blockgraph/BlockWritter.java
@@ -32,6 +32,7 @@ import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.KryoException;
 import com.esotericsoftware.kryo.io.Input;
+import com.example.blockgraph.WalletMetaHelper.WalletMeta;
 
 import info.blockchain.api.blockexplorer.Block;
 
@@ -76,10 +77,6 @@ public class BlockWritter
 	String neo4jBolt = (String) config.getProperty("neo4j.bolt");
 	String neo4jUser = (String) config.getProperty("neo4j.user");
 	String neo4jPassword = (String) config.getProperty("neo4j.password");
-
-	String mysqlJdbcUrl = (String) config.getProperty("mysql.jdbc.url");
-	String mysqlUser = (String) config.getProperty("mysql.jdbc.user");
-	String mysqlPassword = (String) config.getProperty("mysql.jdbc.password");
 
 	final Driver driver = GraphDatabase.driver(neo4jBolt, AuthTokens.basic(neo4jUser, neo4jPassword));
 	Kryo kryo = new Kryo();
@@ -141,201 +138,164 @@ public class BlockWritter
 
 				for (Block block : blockHeightObj.getBlocks())
 				{
-				    try (Connection conn = DriverManager.getConnection(mysqlJdbcUrl, mysqlUser, mysqlPassword))
+
+				    txLoop: for (info.blockchain.api.blockexplorer.Transaction blockTx : block.getTransactions())
 				    {
-					txLoop: for (info.blockchain.api.blockexplorer.Transaction blockTx : block.getTransactions())
+					Map<String, BigDecimal> inputMap = new HashMap<>();
+					for (info.blockchain.api.blockexplorer.Input input : blockTx.getInputs())
 					{
-					    Map<String, BigDecimal> inputMap = new HashMap<>();
-					    for (info.blockchain.api.blockexplorer.Input input : blockTx.getInputs())
-					    {
-						if (input.getPreviousOutput() == null)
-						{
-						    continue txLoop;
-						}
-
-						String addr = input.getPreviousOutput().getAddress();
-						long value = input.getPreviousOutput().getValue();
-						if (addr.trim().isEmpty())
-						{
-						    // Unable to decode address
-						    // e.g.
-						    // 862d8672ffba284095df0228544bdef849ce6fc74b73fc478c01472edc842d04
-						    continue;
-						}
-						if (!inputMap.containsKey(addr))
-						{
-						    inputMap.put(addr, BigDecimal.ZERO);
-						}
-						inputMap.put(addr, inputMap.get(addr).add(BigDecimal.valueOf(value)));
-
-					    }
-
-					    Map<String, BigDecimal> outputMap = new HashMap<>();
-					    for (info.blockchain.api.blockexplorer.Output output : blockTx.getOutputs())
-					    {
-						String addr = output.getAddress();
-						long value = output.getValue();
-						if (addr.trim().isEmpty())
-						{
-						    // Unable to decode address
-						    continue;
-						}
-						if (!outputMap.containsKey(addr))
-						{
-						    outputMap.put(addr, BigDecimal.ZERO);
-						}
-						outputMap.put(addr, outputMap.get(addr).add(BigDecimal.valueOf(value)));
-
-					    }
-
-					    if (inputMap.isEmpty() || outputMap.isEmpty())
+					    if (input.getPreviousOutput() == null)
 					    {
 						continue txLoop;
 					    }
 
-					    BigDecimal totalInput = inputMap.values().stream().reduce(BigDecimal.ZERO, (a, b) -> a.add(b));
-					    BigDecimal totalOutput = outputMap.values().stream().reduce(BigDecimal.ZERO, (a, b) -> a.add(b));
-					    BigDecimal fee = totalInput.subtract(totalOutput);
-					    if (fee.compareTo(BigDecimal.ZERO) > 0)
+					    String addr = input.getPreviousOutput().getAddress();
+					    long value = input.getPreviousOutput().getValue();
+					    if (addr.trim().isEmpty())
 					    {
-						for (String inputAddr : inputMap.keySet())
-						{
-						    BigDecimal amt = inputMap.get(inputAddr);
-						    BigDecimal distributedFee = fee.multiply(amt.divide(totalInput, 19, BigDecimal.ROUND_DOWN));
-						    inputMap.put(inputAddr, amt.subtract(distributedFee));
-						}
+						// Unable to decode address
+						// e.g.
+						// 862d8672ffba284095df0228544bdef849ce6fc74b73fc478c01472edc842d04
+						continue;
 					    }
-
-					    Set<String> intersectedAddresses = new HashSet<>(inputMap.keySet());
-					    intersectedAddresses.retainAll(outputMap.keySet());
-
-					    for (String intersectedAddress : intersectedAddresses)
+					    if (!inputMap.containsKey(addr))
 					    {
-						// check symbol.....
-
-						BigDecimal leftVal = inputMap.get(intersectedAddress);
-						BigDecimal rightVal = outputMap.get(intersectedAddress);
-
-						if (leftVal.compareTo(rightVal) > 0)
-						{
-						    inputMap.put(intersectedAddress, leftVal.subtract(rightVal));
-						    outputMap.remove(intersectedAddress);
-						}
-						else if (leftVal.compareTo(rightVal) < 0)
-						{
-						    inputMap.remove(intersectedAddress);
-						    outputMap.put(intersectedAddress, rightVal.subtract(leftVal));
-						}
-						else
-						{
-						    inputMap.remove(intersectedAddress);
-						    outputMap.remove(intersectedAddress);
-						}
-
+						inputMap.put(addr, BigDecimal.ZERO);
 					    }
+					    inputMap.put(addr, inputMap.get(addr).add(BigDecimal.valueOf(value)));
 
-					    Map<String, BigDecimal> ratioMap = new HashMap<>();
-					    for (Entry<String, BigDecimal> outputEntry : outputMap.entrySet())
+					}
+
+					Map<String, BigDecimal> outputMap = new HashMap<>();
+					for (info.blockchain.api.blockexplorer.Output output : blockTx.getOutputs())
+					{
+					    String addr = output.getAddress();
+					    long value = output.getValue();
+					    if (addr.trim().isEmpty())
 					    {
-						if (totalOutput.compareTo(BigDecimal.ZERO) == 0)
-						{
-						    System.out.println("totalOutput is zero!!!");
-						}
-						BigDecimal ratio = outputEntry.getValue().divide(totalOutput, 19, BigDecimal.ROUND_DOWN);
-						ratioMap.put(outputEntry.getKey(), ratio);
+						// Unable to decode address
+						continue;
 					    }
-
-					    for (Entry<String, BigDecimal> inputEntry : inputMap.entrySet())
+					    if (!outputMap.containsKey(addr))
 					    {
-						String inputAddr = inputEntry.getKey();
-						BigDecimal inputValue = inputEntry.getValue();
-						for (Entry<String, BigDecimal> ratioEntry : ratioMap.entrySet())
-						{
+						outputMap.put(addr, BigDecimal.ZERO);
+					    }
+					    outputMap.put(addr, outputMap.get(addr).add(BigDecimal.valueOf(value)));
 
-						    String outputAddr = ratioEntry.getKey();
-						    if (outputAddr == null || outputAddr.trim().isEmpty())
-						    {
-							System.out.println();
-						    }
+					}
 
-						    BigDecimal weightedOutputValue = inputValue.multiply(ratioEntry.getValue());
+					if (inputMap.isEmpty() || outputMap.isEmpty())
+					{
+					    continue txLoop;
+					}
 
-						    PreparedStatement inputWalletStmt = conn.prepareStatement(
-							    "SELECT w.address,w.wallet_group_name,w.balance,w.incoming_txs,w.last_used_block_number, c.name AS category_name FROM wallet w INNER JOIN wallet_group_category wgc ON wgc.wallet_group_name=w.wallet_group_name INNER JOIN category c ON c.id = wgc.category_id  WHERE address=?");
-						    inputWalletStmt.setString(1, inputAddr);
-						    ResultSet inputRs = inputWalletStmt.executeQuery();
-						    String inputAddressLabels = "";
-						    String inputWalletGroupName = null;
-						    Long inputBalance = null;
-						    Integer inputIncommingTxs = null;
-						    String inputLastedUsedBlockNo = null;
-						    while (inputRs.next())
-						    { 
-							inputWalletGroupName = inputRs.getString("wallet_group_name");
-							inputAddressLabels += ":"+inputRs.getString("category_name");							
-							
-							inputBalance = inputRs.getLong("balance");
-							if(inputRs.wasNull())
-							{
-							    inputBalance = null;
-							}
-							
-							inputIncommingTxs = inputRs.getInt("incoming_txs");
-							if(inputRs.wasNull())
-							{
-							    inputIncommingTxs = null;
-							}
-							
-							inputLastedUsedBlockNo = inputRs.getString("last_used_block_number");
-							
-						    }
-						    
-						    PreparedStatement outputWalletStmt = conn.prepareStatement(
-							    "SELECT w.address,w.wallet_group_name,w.balance,w.incoming_txs,w.last_used_block_number,c.name AS category_name FROM wallet w INNER JOIN wallet_group_category wgc ON wgc.wallet_group_name=w.wallet_group_name INNER JOIN category c ON c.id = wgc.category_id  WHERE address=?");
-						    outputWalletStmt.setString(1, outputAddr);
-						    ResultSet outputRs = outputWalletStmt.executeQuery();
-						    String outputAddressLabels = "";
-						    String outputWalletGroupName = null;
-						    Long outputBalance = null;
-						    Integer outputIncommingTxs = null;
-						    String outputLastedUsedBlockNo = null;
-						    while (outputRs.next())
-						    { 
-							outputWalletGroupName = outputRs.getString("wallet_group_name");
-							outputAddressLabels += ":"+outputRs.getString("category_name");
-							
-							outputBalance = outputRs.getLong("balance");
-							if(outputRs.wasNull())
-							{
-							    outputBalance = null;
-							}
-							
-							outputIncommingTxs = outputRs.getInt("incoming_txs");
-							if(outputRs.wasNull())
-							{
-							    outputIncommingTxs = null;
-							}
-							
-							outputLastedUsedBlockNo = outputRs.getString("last_used_block_number");
-							
-							
-						    }
+					BigDecimal totalInput = inputMap.values().stream().reduce(BigDecimal.ZERO, (a, b) -> a.add(b));
+					BigDecimal totalOutput = outputMap.values().stream().reduce(BigDecimal.ZERO, (a, b) -> a.add(b));
+					BigDecimal fee = totalInput.subtract(totalOutput);
+					if (fee.compareTo(BigDecimal.ZERO) > 0)
+					{
+					    for (String inputAddr : inputMap.keySet())
+					    {
+						BigDecimal amt = inputMap.get(inputAddr);
+						BigDecimal distributedFee = fee.multiply(amt.divide(totalInput, 19, BigDecimal.ROUND_DOWN));
+						inputMap.put(inputAddr, amt.subtract(distributedFee));
+					    }
+					}
 
-						    String query = "MERGE (m:Address"+inputAddressLabels+" {address: '" + inputAddr + "' "+(inputWalletGroupName == null ? "": " ,groupName:'"+inputWalletGroupName+"' "+(inputBalance == null ? "": " ,balance: "+inputBalance)+ (inputIncommingTxs == null ? "": " ,incomingTxs: "+inputIncommingTxs)+ (inputLastedUsedBlockNo == null ? "": " ,lastUsedBlockNumber: "+inputLastedUsedBlockNo)  )+" }) MERGE (n:Address"+outputAddressLabels+" {address:'" + outputAddr
-							    + "' "+(outputWalletGroupName == null ? "": " ,groupName:'"+outputWalletGroupName+"' "+(outputBalance == null ? "": " ,balance: "+outputBalance)+ (outputIncommingTxs == null ? "": " ,incomingTxs: "+outputIncommingTxs)+ (outputLastedUsedBlockNo == null ? "": " ,lastUsedBlockNumber: "+outputLastedUsedBlockNo)  )+" }) MERGE (m)-[:PAY {amount:'" + weightedOutputValue.toPlainString() + "', weight: '"
-							    + ratioEntry.getValue().toPlainString() + "', time:" + blockTx.getTime() + ", txHash:'" + blockTx.getHash()
-							    + "', blockHeight: " + block.getHeight() + ", blockHash:'" + block.getHash() + "'}]->(n)";
-						    System.out.println(query);
-						    tx.run(query);
+					Set<String> intersectedAddresses = new HashSet<>(inputMap.keySet());
+					intersectedAddresses.retainAll(outputMap.keySet());
 
-						}
+					for (String intersectedAddress : intersectedAddresses)
+					{
+					    // check symbol.....
+
+					    BigDecimal leftVal = inputMap.get(intersectedAddress);
+					    BigDecimal rightVal = outputMap.get(intersectedAddress);
+
+					    if (leftVal.compareTo(rightVal) > 0)
+					    {
+						inputMap.put(intersectedAddress, leftVal.subtract(rightVal));
+						outputMap.remove(intersectedAddress);
+					    }
+					    else if (leftVal.compareTo(rightVal) < 0)
+					    {
+						inputMap.remove(intersectedAddress);
+						outputMap.put(intersectedAddress, rightVal.subtract(leftVal));
+					    }
+					    else
+					    {
+						inputMap.remove(intersectedAddress);
+						outputMap.remove(intersectedAddress);
 					    }
 
 					}
-				    }
-				    catch (SQLException e)
-				    {
-					throw new RuntimeException(e);
+
+					Map<String, BigDecimal> ratioMap = new HashMap<>();
+					for (Entry<String, BigDecimal> outputEntry : outputMap.entrySet())
+					{
+					    if (totalOutput.compareTo(BigDecimal.ZERO) == 0)
+					    {
+						System.out.println("totalOutput is zero!!!");
+					    }
+					    BigDecimal ratio = outputEntry.getValue().divide(totalOutput, 19, BigDecimal.ROUND_DOWN);
+					    ratioMap.put(outputEntry.getKey(), ratio);
+					}
+
+					for (Entry<String, BigDecimal> inputEntry : inputMap.entrySet())
+					{
+					    String inputAddr = inputEntry.getKey();
+					    BigDecimal inputValue = inputEntry.getValue();
+
+					    WalletMeta inputWallet = null;
+					    try
+					    {
+						inputWallet = new WalletMetaHelper(config, inputAddr).getMeta();
+						
+					    }
+					    catch (IOException | SQLException e)
+					    {
+						continue;
+					    }
+
+					    for (Entry<String, BigDecimal> ratioEntry : ratioMap.entrySet())
+					    {
+
+						String outputAddr = ratioEntry.getKey();
+						if (outputAddr == null || outputAddr.trim().isEmpty())
+						{
+						    System.out.println();
+						}
+
+						BigDecimal weightedOutputValue = inputValue.multiply(ratioEntry.getValue());
+
+						
+						WalletMeta outputWallet = null;
+						try
+						{
+						    outputWallet = new WalletMetaHelper(config, outputAddr).getMeta();
+						}
+						catch (IOException | SQLException e)
+						{
+						    continue;
+						}
+
+						String query = "MERGE (m:Address" + (inputWallet.getCategory() == null ? "" : ":" + inputWallet.getCategory() + " ")
+							+ " {address: '" + inputAddr + "' "
+							+ (inputWallet.getGroup() == null ? "" : " ,groupName:'" + inputWallet.getGroup() + "' ") + " ,balance: "
+							+ inputWallet.getBalance()
+
+							+ " }) MERGE (n:Address" + (outputWallet.getCategory() == null ? "" : ":" + outputWallet.getCategory() + " ")
+							+ " {address:'" + outputAddr + "' "
+							+ (outputWallet.getGroup() == null ? "" : " ,groupName:'" + outputWallet.getGroup() + "' ") + " ,balance: "
+							+ outputWallet.getBalance() + " }) MERGE (m)-[:PAY {amount:'" + weightedOutputValue.toPlainString() + "', weight: '"
+							+ ratioEntry.getValue().toPlainString() + "', time:" + blockTx.getTime() + ", txHash:'" + blockTx.getHash()
+							+ "', blockHeight: " + block.getHeight() + ", blockHash:'" + block.getHash() + "'}]->(n)";
+						System.out.println(query);
+						tx.run(query);
+
+					    }
+					}
+
 				    }
 
 				}
---
+++ src/main/java/com/example/blockgraph/WalletMetaHelper.java
@@ -0,0 +1,128 @@
+package com.example.blockgraph;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import info.blockchain.api.statistics.Statistics;
+import software.amazon.ion.Decimal;
+
+public class WalletMetaHelper
+{
+    private static Map<String, String> cateMap;
+    private String address;
+    private Configuration config;
+
+    public WalletMetaHelper(Configuration config, String address)
+    {
+	this.config = config;
+	this.address = address;
+    }
+
+    public WalletMeta getMeta() throws IOException, SQLException
+    {
+	WalletMeta retVal = new WalletMeta();
+	retVal.setAddress(address);
+	Document doc = Jsoup.connect("https://www.walletexplorer.com/address/" + address).validateTLSCertificates(false).get();
+
+	String group = doc.select("#main > h2 > div > a").get(0).text().trim();
+	retVal.setGroup(group);
+
+	if (cateMap == null)
+	{
+	    cateMap = new HashMap<>();
+	    String mysqlJdbcUrl = (String) config.getProperty("mysql.jdbc.url");
+	    String mysqlUser = (String) config.getProperty("mysql.jdbc.user");
+	    String mysqlPassword = (String) config.getProperty("mysql.jdbc.password");
+	    try (Connection conn = DriverManager.getConnection(mysqlJdbcUrl, mysqlUser, mysqlPassword))
+	    {
+		PreparedStatement outputWalletStmt = conn
+			.prepareStatement("SELECT wgc.wallet_group_name,c.name AS category_name FROM wallet_group_category wgc  INNER JOIN category c ON c.id = wgc.category_id");
+
+		ResultSet rs = outputWalletStmt.executeQuery();
+
+		while (rs.next())
+		{
+		    cateMap.put(rs.getString("wallet_group_name"), rs.getString("category_name"));
+		}
+	    }
+
+	}
+	
+	String category = cateMap.get(group);
+	retVal.setCategory(category);
+	
+	String balanceStr = doc.select("#main > table > tbody > tr:nth-child(2) > td:nth-child(3)").get(0).text().trim();
+	BigDecimal balance =new BigDecimal(balanceStr).multiply(Decimal.valueOf("100000000"));
+	retVal.setBalance(balance.intValue());
+	
+	return retVal;
+
+    }
+
+    public static class WalletMeta
+    {
+	private String address;
+	private long balance;
+	private String category;
+	private String group;
+	
+	
+
+	public String getAddress()
+	{
+	    return address;
+	}
+
+	public void setAddress(String address)
+	{
+	    this.address = address;
+	}
+
+	public long getBalance()
+	{
+	    return balance;
+	}
+
+	public void setBalance(long balance)
+	{
+	    this.balance = balance;
+	}
+
+	public String getCategory()
+	{
+	    return category;
+	}
+
+	public void setCategory(String category)
+	{
+	    this.category = category;
+	}
+
+	public String getGroup()
+	{
+	    return group;
+	}
+
+	public void setGroup(String group)
+	{
+	    this.group = group;
+	}
+
+    }
+}
