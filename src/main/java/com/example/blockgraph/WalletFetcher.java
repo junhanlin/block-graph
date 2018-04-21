3b87af9b2cbb017e000f935e74a094c94f52d3df
JohnLin <john.lin0420@gmail.com>
2018-04-22 01:42:51 +0800

    cl

--- src/main/java/com/example/blockgraph/WalletFetcher.java
+++ src/main/java/com/example/blockgraph/WalletFetcher.java
@@ -23,49 +23,5 @@ import info.blockchain.api.blockexplorer.BlockExplorer;
 
 public class WalletFetcher
 {
-    Parameters params = new Parameters();
-    // Read data from this file
-    File propertiesFile = new File(args[0]);
-
-    FileBasedConfigurationBuilder<FileBasedConfiguration> builder = new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
-	    .configure(params.fileBased().setFile(propertiesFile));
-    Configuration config = builder.getConfiguration();
-
-    String awsAccountKey = (String) config.getProperty("aws.account.key");
-    String awsAccountSecret = (String) config.getProperty("aws.account.secret");
-    String bucketName = (String) config.getProperty("aws.s3.bucket");
-
-    AmazonS3Client s3Client = new AmazonS3Client(new BasicAWSCredentials(awsAccountKey, awsAccountSecret));
-
-    Kryo kryo = new Kryo();
-    BlockExplorer blockExplorer = new BlockExplorer();
-
-    long blockHeight = Long.parseLong(args[1]);
-
-    while(blockHeight>0)
-    {
-	System.out.println("Fetching block at height " + blockHeight);
-	List<Block> blocks = blockExplorer.getBlocksAtHeight(blockHeight);
-
-	ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-
-	try (com.esotericsoftware.kryo.io.Output kryoOutput = new com.esotericsoftware.kryo.io.Output(byteOut))
-	{
-	    BlockHeight blockHeightObj = new BlockHeight();
-	    blockHeightObj.setBlocks(blocks);
-	    kryo.writeObject(kryoOutput, blockHeightObj);
-
-	}
-
-	try (ByteArrayInputStream byteIn = new ByteArrayInputStream(byteOut.toByteArray()))
-	{
-	    PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, "height-" + blockHeight, byteIn, new ObjectMetadata());
-
-	    PutObjectResult putObjectResult = s3Client.putObject(putObjectRequest);
-
-	}
-	System.out.println("Done!");
-	blockHeight--;
-	Thread.sleep(600);
-    }
+   
 }
