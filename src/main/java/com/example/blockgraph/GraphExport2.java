ebee947fe83a6cc52515c100a39b0f64dbc86067
JohnLin <john.lin0420@gmail.com>
2018-04-22 01:40:34 +0800

    new export 2

---
+++ src/main/java/com/example/blockgraph/GraphExport2.java
@@ -0,0 +1,108 @@
+package com.example.blockgraph;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.types.Node;
+
+public class GraphExport2
+{
+    public static void main(String[] args) throws ConfigurationException, IOException
+    {
+	Parameters parameters = new Parameters();
+	File propertiesFile = new File(args[0]);
+
+	FileBasedConfigurationBuilder<FileBasedConfiguration> builder = new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+		.configure(parameters.fileBased().setFile(propertiesFile));
+	Configuration config = builder.getConfiguration();
+
+	String neo4jBolt = (String) config.getProperty("neo4j.bolt");
+	String neo4jUser = (String) config.getProperty("neo4j.user");
+	String neo4jPassword = (String) config.getProperty("neo4j.password");
+
+	try (PrintWriter writer = new PrintWriter(new FileWriter(args[1])))
+	{
+	    writer.println("address,group,category,balance");
+	    final Driver driver = GraphDatabase.driver(neo4jBolt, AuthTokens.basic(neo4jUser, neo4jPassword));
+	    try (Session session = driver.session())
+	    {
+		StatementResult stmtRes = session.run("MATCH (n:Address) RETURN n");
+		while (stmtRes.hasNext())
+		{
+		    Record rec = stmtRes.next();
+		    Node node = rec.get("n").asNode();
+		    Map<String, Object> propMap = node.asMap();
+		    String category = "OTHER";
+		    if(node.hasLabel("Exchanges"))
+		    {
+			category = "Exchanges";
+		    }
+		    else if(node.hasLabel("Gambling"))
+		    {
+			category = "Gambling";
+		    }
+		    else if(node.hasLabel("Old"))
+		    {
+			category = "Old";
+		    }
+		    else if(node.hasLabel("Pools"))
+		    {
+			category = "Pools";
+		    }
+		    else if(node.hasLabel("Services"))
+		    {
+			category = "Services";
+		    }
+		    
+		    writer.print(propMap.get("address"));
+		    writer.print("," + propMap.get("group"));
+		    writer.print("," + category);
+		    writer.print("," + propMap.get("balance"));
+		    writer.println();
+
+		}
+	    }
+	}
+
+	try (PrintWriter writer = new PrintWriter(new FileWriter(args[2])))
+	{
+	    writer.println("fromAddress,toAddress,amount,weight,time,blockHeight,txHash");
+	    final Driver driver = GraphDatabase.driver(neo4jBolt, AuthTokens.basic(neo4jUser, neo4jPassword));
+	    try (Session session = driver.session())
+	    {
+		StatementResult stmtRes = session.run("MATCH (m:Address)-[p:PAY]->(n:Address) RETURN m,p,n");
+		while (stmtRes.hasNext())
+		{
+
+		    Record rec = stmtRes.next();
+
+		    Map<String, Object> propMap = rec.get("p").asRelationship().asMap();
+		    writer.print(rec.get("m").asNode().asMap().get("address"));
+		    writer.print("," + rec.get("n").asNode().asMap().get("address"));
+		    writer.print("," + propMap.get("amount"));
+		    writer.print("," + propMap.get("weight"));
+		    writer.print("," + propMap.get("time"));
+		    writer.print("," + propMap.get("blockHeight"));
+		    writer.print("," + propMap.get("txHash"));
+		    writer.println();
+
+		}
+	    }
+	}
+    }
+}
---
+++ src/main/java/com/example/blockgraph/WalletFetcher.java
@@ -0,0 +1,71 @@
+package com.example.blockgraph;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.List;
+
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.esotericsoftware.kryo.Kryo;
+
+import info.blockchain.api.blockexplorer.Block;
+import info.blockchain.api.blockexplorer.BlockExplorer;
+
+public class WalletFetcher
+{
+    Parameters params = new Parameters();
+    // Read data from this file
+    File propertiesFile = new File(args[0]);
+
+    FileBasedConfigurationBuilder<FileBasedConfiguration> builder = new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+	    .configure(params.fileBased().setFile(propertiesFile));
+    Configuration config = builder.getConfiguration();
+
+    String awsAccountKey = (String) config.getProperty("aws.account.key");
+    String awsAccountSecret = (String) config.getProperty("aws.account.secret");
+    String bucketName = (String) config.getProperty("aws.s3.bucket");
+
+    AmazonS3Client s3Client = new AmazonS3Client(new BasicAWSCredentials(awsAccountKey, awsAccountSecret));
+
+    Kryo kryo = new Kryo();
+    BlockExplorer blockExplorer = new BlockExplorer();
+
+    long blockHeight = Long.parseLong(args[1]);
+
+    while(blockHeight>0)
+    {
+	System.out.println("Fetching block at height " + blockHeight);
+	List<Block> blocks = blockExplorer.getBlocksAtHeight(blockHeight);
+
+	ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+
+	try (com.esotericsoftware.kryo.io.Output kryoOutput = new com.esotericsoftware.kryo.io.Output(byteOut))
+	{
+	    BlockHeight blockHeightObj = new BlockHeight();
+	    blockHeightObj.setBlocks(blocks);
+	    kryo.writeObject(kryoOutput, blockHeightObj);
+
+	}
+
+	try (ByteArrayInputStream byteIn = new ByteArrayInputStream(byteOut.toByteArray()))
+	{
+	    PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, "height-" + blockHeight, byteIn, new ObjectMetadata());
+
+	    PutObjectResult putObjectResult = s3Client.putObject(putObjectRequest);
+
+	}
+	System.out.println("Done!");
+	blockHeight--;
+	Thread.sleep(600);
+    }
+}
